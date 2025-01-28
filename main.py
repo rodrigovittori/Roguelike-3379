f852758d25fd9834ea70d4ce22136df838576557
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2025-01-27 21:01:39 -0300

    [M7.L1] - Actividad #2: "Creando un mapa"
    Ampliamos el mapa y agregamos una representación de tablas en forma de listas

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 #pgzero
 
 """
-Version actual: [M7.L1] - Actividad #1 "Ventana de juego celular"
+Version actual: [M7.L1] - Actividad #2: "Creando un mapa"
 
-Objetivo: Explicar el sistema de ventanas/mapas escalados por tiles (celdas)
+Objetivo: Comprender como modificar el tamaño del mapa y como vamos a cargar nuestros mapas (tablas)
 
+Paso Nº 1: Modificar las variables que definen la cant de casillas horiz y vert (7x7)
+Paso Nº 2: Crear una "tabla" (lista de listas) que represente nuestro mapa
+
+Nota: en el próximo ejercicio es que se crean los actores para dibujar
+
+=========================================================================================
 Pack Kodland: https://kenney.nl/assets/roguelike-caves-dungeons (NO VIENE PRECORTADO)
 packs de assets: https://kenney.nl/assets/series:Tiny?sort=update (LO TIENEN QUE ESCALAR)
-
 pack escalado (drive del profe): https://drive.google.com/drive/folders/19obh4TK0RIBWlXOsaOq9uJ287jUHuLTn?usp=drive_link
+
+> Para redimensionar assets https://imageresizer.com/bulk-resize/
 """
 
 # Ventana de juego hecha de celdas
 celda = Actor('border') # Celda que voy a utilizar como referencia para mi mapa
 
-cant_celdas_ancho = 5 # Ancho del mapa (en celdas)
-cant_celdas_alto = 5 # Altura del mapa (en celdas)
+cant_celdas_ancho = 7 # Ancho del mapa (en celdas)
+cant_celdas_alto = 7 # Altura del mapa (en celdas)
 
 WIDTH =  celda.width  * cant_celdas_ancho # Ancho de la ventana (en píxeles)
 HEIGHT = celda.height * cant_celdas_alto  # Alto de la ventana (en píxeles)
 
 TITLE = "Rogue-like: Mazmorra Maldita" # Título de la ventana de juego
 FPS = 30 # Número de fotogramas por segundo
+
+mapa = [ [0, 0, 0, 0, 0, 0, 0],
+         [0, 1, 2, 1, 3, 1, 0],
+         [0, 1, 1, 2, 1, 1, 0],
+         [0, 3, 2, 1, 1, 3, 0],
+         [0, 1, 1, 1, 3, 1, 0],
+         [0, 1, 3, 1, 1, 2, 0],
+         [0, 0, 0, 0, 0, 0, 0] ]
 
 def draw():
     screen.fill((200,200,200))
