108ef8b862c9222a4707a3184f5525af890487c9
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2025-02-03 20:44:05 -0300

    [M7.L2] - Actividad #2 "Generando enemigos"
    Agregar lógica de spawn para enemigos, y validación de su posición asignada

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 #pgzero
 
 """
-Version actual: [M7.L1] - Actividades Extra
-Objetivo del ejercicio: Ampliar mapa, agregar fila extra y reubicar datos en pantalla
+Version actual: [M7.L2] - Actividad #2 "Generando enemigos"
+Objetivo del ejercicio: Agregar generación aleatoria de enemigos con atributos randomizados
 
-Nota: Nuestro código ya cumple la primer actividad extra
+NOTA: La Actividad # 1 es actualizar el mapa de sueños
 
 Pasos:
-#1: Modificamos nuestro mapa para que sea de 9x9 casillas, más una fila extra para mostrar los atributos del jugador
-#2: Modificamos nuestros mapas
-#3: Agregamos el color de relleno de la pantalla en nuestro draw()
-#4: Modificamos los screen.draw.text()
-#5: Modificamos los límites de restricción de movimiento (sólo hacia abajo)
+#1: Importar random
+#2: creamos una constante que determine la cantidad de enemigos a spawnear (5)
+#3: Creamos un bucle FOR donde calculamos la posición, la validamos,
+    creamos los actores enemigos y les asignamos su salud y ataque con valores randomizados
+#4: Agregamos un bucle FOR en nuestro draw() para mostrar los enemigos en pantalla
+
+Nota: Pronto calcularemos las colisiones contra ellos
 
 =========================================================================================
 Pack Kodland: https://kenney.nl/assets/roguelike-caves-dungeons (NO VIENE PRECORTADO)
@@ -20,6 +22,8 @@ pack escalado (drive del profe): https://drive.google.com/drive/folders/19obh4TK
 
 > Para redimensionar assets https://imageresizer.com/bulk-resize/
 """
+
+import random
 
 # Ventana de juego hecha de celdas
 celda = Actor('border') # Celda que voy a utilizar como referencia para mi mapa
@@ -53,6 +57,41 @@ personaje.salud = 100
 #       (también pueden implementar un sistema de miss y critical hits)
 personaje.ataque = 5
 
+# Corregimos spawn del Jugador:
+personaje.top = celda.height
+personaje.left = celda.width
+
+""" ******************************************************************* """
+################ VARIABLES ################
+
+CANT_ENEMIGOS_A_SPAWNEAR = 5
+
+lista_enemigos = []
+
+""" ******************************************************************* """
+################## GENERAR ENEMIGOS ##################
+
+# To-Do: migrar a función
+while (len(lista_enemigos) < CANT_ENEMIGOS_A_SPAWNEAR):
+    x = (random.randint(2, cant_celdas_ancho - 2) * celda.width)
+    y = (random.randint(2, cant_celdas_alto - 3) * celda.height)
+    # To-Do: Agregar variable para determinar tipo de enemigo a spawnear
+    
+    nvo_enemigo = Actor("enemy", topleft = (x, y))
+
+    # Validamos que los enemigos no spawneen uno sobre el otro
+    posicion_duplicada = False
+    for enemigo in lista_enemigos:
+        if (nvo_enemigo.pos == enemigo.pos): # Si la posición de nvo_enemigo es IGUAL a la de CUALQUIER enemigo en la lista,
+            posicion_duplicada = True        # Actualizamos la flag que indica que la posicion está duplicada
+    if (posicion_duplicada):
+        continue
+    else:
+        # Si NO hay conflicto: randomizamos salud, ataque y lo agregamos a lista_enemigos
+        nvo_enemigo.salud = random.randint(10, 20)
+        nvo_enemigo.ataque = random.randint(5, 10)
+        lista_enemigos.append(nvo_enemigo)
+        
 ################## MAPAS ##################
 
 mapa =   [[0, 0, 0, 0, 0, 0, 0, 0, 0], 
@@ -131,6 +170,10 @@ def dibujar_mapa(mapa):
 def draw():
     screen.fill("#2f3542") # rgb = (47, 53, 66)
     dibujar_mapa(mapa_actual)
+
+    for enemigo in lista_enemigos:
+        enemigo.draw()
+    
     personaje.draw()
     
     # Mostramos valores personaje:
